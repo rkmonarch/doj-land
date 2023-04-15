adcbb88c5159c2cf84389e30274f83399f750438
rkmonarch <rahulkulkarni5234@gmail.com>
2023-04-15 14:10:48 +0530

    Added home page

---
+++ src/components/footer.tsx
@@ -0,0 +1,31 @@
+import { AiFillGithub } from "react-icons/ai";
+
+const Footer = () => {
+  return (
+    <footer className="w-full p-4 sm:p-6 sm:px-4 bg-gray-700 bg-opacity-30">
+      <div className="mx-auto max-w-[1080px]">
+        <div className="sm:flex sm:items-center sm:justify-between">
+          <span className="text-md sm:text-center text-gray-400">
+            © {new Date().getFullYear()}{" "}
+            <a href="" className="hover:underline">
+              DojLand
+            </a>
+            . All Rights Reserved.
+          </span>
+          <div className="flex mt-4 space-x-6 sm:justify-center sm:mt-0">
+            <a
+              href="https://github.com/neel-ds/5ire-land"
+              target="_blank"
+              className="text-gray-50 hover:text-white"
+              rel="noreferrer"
+            >
+              <AiFillGithub size={25} />
+            </a>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,124 +1,112 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import Head from "next/head";
+import Link from "next/link";
+import Image from "next/image";
+import { NextPage } from "next";
+import {
+  BsFillFileEarmarkLockFill,
+  BsFillCheckCircleFill,
+} from "react-icons/bs";
+import { MdSecurity } from "react-icons/md";
 
-const inter = Inter({ subsets: ['latin'] })
-
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+interface FeatureCardProps {
+  title: string;
+  desc: string;
+  icon: any;
 }
+
+const FeatureCard = ({ title, desc, icon }: FeatureCardProps) => {
+  return (
+    <div className="relative p-5 rounded-lg bg-white/40 bg-opacity-20 dark:bg-gray-700 dark:bg-opacity-30">
+      <dt className="flex flex-col items-center md:items-start">
+        <div className="flex items-center justify-center h-12 w-12 rounded-md bg-[#732fff] dark:bg-[#a13bf7] text-white">
+          {icon}
+        </div>
+        <p className="pt-5 text-lg leading-6 font-medium font-semibold text-[#732fff] dark:text-[#e99aff]">
+          {title}
+        </p>
+      </dt>
+      <dd className="mt-2 text-base text-center text-gray-600 md:text-left dark:text-white">
+        {desc}
+      </dd>
+    </div>
+  );
+};
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>DojLand</title>
+        <meta name="description" content="DojLand" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className="px-4 md:px-0 mx-auto max-w-[1080px]">
+        <div className="md:text-left h-[calc(100vh-60px)] flex justify-center flex-row">
+          <div className="flex flex-col justify-center">
+            <h1 className="text-4xl font-extrabold tracking-tight text-gray-900 dark:text-white sm:text-5xl md:text-6xl">
+              <span className="block text-gray-700 xl:inline dark:text-white">
+                Welcome to
+              </span>
+              <span className="block text-transparent bg-clip-text bg-gradient-to-r from-[#8b00ff] to-[#a75eff] dark:from-[#8c2bff] dark:to-[#d97dff] pb-4">
+                Doj Land
+              </span>
+              <span className="block font-semibold text-gray-700 dark:text-[#e99aff] font-medium text-2xl">
+                Decentralized Application
+              </span>
+            </h1>
+            <p className="mt-3 text-base text-gray-600 dark:text-gray-300 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
+              Get your Records of Right on-chain with sustainable approach.
+            </p>
+            <div className="mt-5 sm:mt-8 sm:flex lg:justify-start md:flex-col lg:flex-row">
+              <div>
+                <Link
+                  href="/explore"
+                  className="w-full md:w-[70%] lg:w-full flex items-center justify-center px-8 py-3 border-0 border-transparent text-base font-medium rounded-3xl text-white bg-gradient-to-r from-[#a13bf7] to-[#7500ff] hover:drop-shadow-[0_3px_5px_#7d7d7d] dark:hover:drop-shadow-[0_3px_5px_#8ce1ff] md:py-2 md:text-lg md:px-8"
+                >
+                  Explore
+                </Link>
+              </div>
+            </div>
+          </div>
+          <div className="md:flex hidden my-auto w-[30%] md:w-[60%] ml-10 items-end">
+            <Image
+              src="/vector.png"
+              width="600"
+              height="500"
+              className="ml-10"
+              alt="Banner"
+            />
+          </div>
+        </div>
+
+        <div className="pt-5 pb-20 mx-auto max-w-7xl">
+          <div className="flex flex-col w-full mb-5 text-center md:mb-10">
+            <h1 className="text-4xl mb-10 font-bold title-font mb-4 text-[#732fff] dark:text-white">
+              Features
+            </h1>
+          </div>
+          <dl className="space-y-10 md:space-y-0 md:grid md:grid-cols-3 md:gap-x-8 md:gap-y-10">
+            <FeatureCard
+              icon={<BsFillFileEarmarkLockFill size={25} />}
+              title="Middle-Ground Layer"
+              desc="Dojima Network is built on a multi-layer architecture that consists of two blockchains working together in a decentralized manner with all other blockchains to both access liquidity and acts as a cross-chain platform for developers to develop cross-chain applications."
+            />
+            <FeatureCard
+              icon={<MdSecurity size={25} />}
+              title="Security"
+              desc="Everything on-chain! Prevent any forgery and errors while verifying a claim. No one can change RoR except the authorized person."
+            />
+            <FeatureCard
+              icon={<BsFillCheckCircleFill size={25} />}
+              title="Verification"
+              desc="It also ensures that the right person is performing transaction or deal of the land. User can also transfer ownership after KYC process."
+            />
+          </dl>
+        </div>
+      </main>
+    </>
+  );
+};
+
+export default Home;
